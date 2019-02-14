369f5a2de833e263ae967f8bdd9d060c3b1be80a
CaioAlarcon <42846850+CaioAlarcon@users.noreply.github.com>
2019-02-14 13:09:14 -0200

    Update lamp.sh

--- LAMP/lamp.sh
+++ LAMP/lamp.sh
@@ -1,7 +1,7 @@
-#apt-get update
-#apt-get upgrade
-#apt-get install apache2
-#apt-get install mysql-server
-#apt-get install php-pear php-fpm php-dev php-zip php-curl php-xmlrpc php-gd php-mysql php-mbstring php-xml libapache2-mod-php
-#sudo service apache2 restart
-#php -r 'echo "\n\nYour PHP installation is working fine.\n\n\n";'
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install apache2
+sudo apt-get install mysql-server
+sudo apt-get install php-pear php-fpm php-dev php-zip php-curl php-xmlrpc php-gd php-mysql php-mbstring php-xml libapache2-mod-php
+sudo service apache2 restart
+php -r 'echo "\n\nYour PHP installation is working fine.\n\n\n";'
