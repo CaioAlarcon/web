ae67910a844de29bcafc8f1e22677ff229e65c56
CaioAlarcon <42846850+CaioAlarcon@users.noreply.github.com>
2019-02-14 13:03:58 -0200

    Create lamp.sh

---
+++ LAMP/lamp.sh
@@ -0,0 +1,7 @@
+#apt-get update
+#apt-get upgrade
+#apt-get install apache2
+#apt-get install mysql-server
+#apt-get install php-pear php-fpm php-dev php-zip php-curl php-xmlrpc php-gd php-mysql php-mbstring php-xml libapache2-mod-php
+#sudo service apache2 restart
+#php -r 'echo "\n\nYour PHP installation is working fine.\n\n\n";'
