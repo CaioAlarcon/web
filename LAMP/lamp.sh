279af0a1f603eed73e3260d08282ae1affb1dbdb
CaioAlarcon <42846850+CaioAlarcon@users.noreply.github.com>
2019-02-14 13:25:27 -0200

    Update lamp.sh

--- LAMP/lamp.sh
+++ LAMP/lamp.sh
@@ -4,4 +4,5 @@ sudo apt-get install apache2
 sudo apt-get install mysql-server
 sudo apt-get install php-pear php-fpm php-dev php-zip php-curl php-xmlrpc php-gd php-mysql php-mbstring php-xml libapache2-mod-php
 sudo service apache2 restart
+sudo chown www-data:www-data /var/www/html
 php -r 'echo "\n\nYour PHP installation is working fine.\n\n\n";'
