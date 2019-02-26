7e715c860819bdf1b586578f17fac5b2829bbf6e
CaioAlarcon <42846850+CaioAlarcon@users.noreply.github.com>
2019-02-25 23:28:24 -0300

    Update WPCONFIG.sh
    voltei pro script antigo pq as alterações que eu fiz nele eram um lixo

--- WORDPRESS/WPCONFIG.sh
+++ WORDPRESS/WPCONFIG.sh
@@ -12,34 +12,28 @@ echo "GRANT ALL PRIVILEGES ON $dbname.* TO '$dbuser'@'localhost';" | mysql -u ro
 echo "FLUSH PRIVILEGES;" | mysql -u root -p$rootpass
 echo "New MySQL database is successfully created"
 # Download, unpack and configure WordPress
-wget -q -O - "http://wordpress.org/latest.tar.gz" | tar -xzf - -C /var/www --transform s/wordpress/html/
-chown www-data: -R /var/www/html && cd /var/www/html
+read -r -p "Enter your WordPress URL? [e.g. mywebsite.com]: " wpURL
+wget -q -O - "http://wordpress.org/latest.tar.gz" | tar -xzf - -C /var/www --transform s/wordpress/$wpURL/
+chown www-data: -R /var/www/$wpURL && cd /var/www/$wpURL
 cp wp-config-sample.php wp-config.php
 chmod 640 wp-config.php
 mkdir uploads
 sed -i "s/database_name_here/$dbname/;s/username_here/$dbuser/;s/password_here/$userpass/" wp-config.php
 # Create Apache virtual host
 echo "
-ServerName html
-ServerAlias www.html
-DocumentRoot /var/www/html
+ServerName $wpURL
+ServerAlias www.$wpURL
+DocumentRoot /var/www/$wpURL
 DirectoryIndex index.php
 Options FollowSymLinks
 AllowOverride All
 ErrorLog ${APACHE_LOG_DIR}/error.log
 CustomLog ${APACHE_LOG_DIR}/access.log combined
-" > /etc/apache2/sites-available/html
+" > /etc/apache2/sites-available/$wpURL
 # Enable the site
-a2ensite html
-chown www-data:www-data /var/www/html
-chmod -R  777 /var/www/html
-echo "max_execution_time = 300" >> /etc/php/7.0/apache2/php.ini 
-echo "post_max_size = 2G" >> /etc/php/7.0/apache2/php.ini 
-echo "upload_max_filesize = 2G" >> /etc/php/7.0/apache2/php.ini 
+a2ensite $wpURL
 service apache2 restart
 # Output
-WPVER=$(grep "wp_version = " /var/www/html/wp-includes/version.php |awk -F\' '{print $2}')
+WPVER=$(grep "wp_version = " /var/www/$wpURL/wp-includes/version.php |awk -F\' '{print $2}')
 echo -e "\nWordPress version $WPVER is successfully installed!"
-echo "Agora, antes de instalar o WP, muda a config do PHP para aumentar o upload e o POST"
-php -i | grep "Loaded Configuration File"
-
+echo -en "\aPlease go to http://$wpURL and finish the installation\n"
